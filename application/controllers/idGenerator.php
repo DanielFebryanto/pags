c8df4a65985380f0c95e7b938ffc8a36d984e298
daniel <daniel.febryanto@gmail.com>
2017-01-21 12:58:47 +0700

    ,
    .

--- application/controllers/idGenerator.php
+++ application/controllers/idGenerator.php
@@ -4,10 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class IdGenerator extends CI_Controller {
 
 	function generateId($key){
-		//md5($this->input->post('password'))
-		//$this->db->where('password', );
-		$f = md5('daniel');
+		$toCrypt = md5($key);
 		
-		echo $f;
+		return $toCrypt;
 	}
 }
--- application/libraries/template.php
+++ application/libraries/template.php
@@ -4,9 +4,11 @@ class Template extends CI_Controller{
     protected $_ci;
     function __construct(){
         $this->_ci= &get_instance();
+        $this->load->model(array('userRoleModel', 'serviceModel'));
     }
     
     function display($template, $data=null){
+        
         $data['_content']=$this->_ci->load->view($template,$data, true);
         $data['_header']=$this->_ci->load->view('header',$data, true);
         $data['_sidebar']=$this->_ci->load->view('side_bar',$data, true);
